956f8a4641dc026ce1ccb9c5f0a6d11161015249
qudwns2052 <qudwns2052@gmail.com>
2020-01-28 03:58:40 +0900

    200128

--- crawling_class.py
+++ crawling_class.py
@@ -18,18 +18,18 @@ class instagram_crawler :
         self.collected_url = None
         self.data = None
 
-    def set_url(url) :
+    def set_url(self, url) :
         self.url = url
 
-    #게시글 url을 모두 수집
-    def collect_url(self) :
+    #게시글 url을 수집
+    def collect_url(self, count) :
         url = self.url
         driver = webdriver.Chrome('chromedriver.exe')
         driver.get(url)
         sleep(5)
-
         SCROLL_PAUSE_TIME = 1.0
         reallink = []
+        finish = False
 
         while True :
             pageString = driver.page_source
@@ -40,24 +40,30 @@ class instagram_crawler :
                     title = link.select('a')[i]
                     real = title.attrs['href']
                     reallink.append(real)
+                
+                count = count - 3
+                if(count <= 0) :
+                    finish = True
+                    break
+            
+            if(finish == True) :
                 break
-            break
 
-            # last_height = driver.execute_script("return document.body.scrollHeight")
-            # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            # sleep(SCROLL_PAUSE_TIME)
-            # new_height = driver.execute_script("return document.body.scrollHeight")
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(SCROLL_PAUSE_TIME)
+            new_height = driver.execute_script("return document.body.scrollHeight")
 
-            # if new_height == last_height :
-            #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            #     sleep(SCROLL_PAUSE_TIME)
-            #     new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height :
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                sleep(SCROLL_PAUSE_TIME)
+                new_height = driver.execute_script("return document.body.scrollHeight")
 
-            #     if new_height == last_height :
-            #         break
-            #     else :
-            #         last_height = new_height
-            #         continue
+                if new_height == last_height :
+                    break
+                else :
+                    last_height = new_height
+                    continue
 
 
         reallinknum = len(reallink)
@@ -70,7 +76,7 @@ class instagram_crawler :
     def make_data(self) :
 
         #쓸모없는 해시태그 list를 만들어서, 정규표현식으로 추후 해시태그 필터
-        trash_list = ['반사', '팔', 'fff', 'f4f', 'follow', 'like', '일상', 
+        trash_list = ['반사', '팔', 'fff', 'f4f', 'follow', 'like', '일상', '스타', '그램', 'lfl', '좋튀', '댓글', '음식',
                     'l4f', '좋반', '데일리', '셀카', '소통', '하면', '하자', '오오디디', 'oodd', '환영']
         csvtext = []
         driver = webdriver.Chrome('chromedriver.exe')
@@ -87,6 +93,7 @@ class instagram_crawler :
             date = soup.select('time')[1]
             date = date.attrs['datetime']
             csvtext[i].append(date)
+            max_cnt = 0
 
             #게시글의 hashtag 가져오기
             for hashtag in soup.find_all("meta", attrs={"property":"instapp:hashtags"}) :
@@ -98,8 +105,16 @@ class instagram_crawler :
                         break
                 if not is_trash :
                     csvtext[i].append(hashtag)
+                    max_cnt += 1
+
+                if max_cnt >= 3 :
+                    break
+            while(max_cnt < 3) :
+                csvtext[i].append("0")
+                max_cnt += 1
 
             #게시글의 imglink 가져오기
+            max_cnt = 0
             while(1) :
                 sleep(1)
                 pageString = driver.page_source
@@ -107,26 +122,34 @@ class instagram_crawler :
                 imgs = soup.select('img')[1]
                 imgs = imgs.attrs['src']
                 csvtext[i].append(imgs)
+                max_cnt += 1
+
+                if max_cnt >= 3 :
+                    break
                 try :
                     driver.find_element_by_class_name("coreSpriteRightChevron").click()
                 except NoSuchElementException :
                     break
-        
+
+            while(max_cnt < 3) :
+                csvtext[i].append("0")
+                max_cnt += 1
+
         self.data = pd.DataFrame(csvtext)
         driver.close()
 
-    def get_collected_url() :
+    def get_collected_url(self) :
         return self.collected_url
 
-    def get_data() :
+    def get_data(self) :
         return self.data
 
-    def save_data(filename) :
+    def save_data(self, filename) :
         self.data.to_csv(filename, encoding="utf-8-sig")
         print("저장완료")
 
     #list에 담겨있는 이미지 링크들을 다운로드하는 함수
-    def download_img(img_url_list) :
+    def download_img(self, img_url_list) :
         for i in range(img_url_list) :
             urlretrieve(img_url_list[i], str(i) + ".jpg")
 
@@ -134,7 +157,13 @@ class instagram_crawler :
 if __name__ == '__main__' :
 
     crawler = instagram_crawler()
-    crawler.set_url("https://instagram.com/explore/tags/음식/")
-    crawler.collect_url()
+
+    keyword = input("크롤링할 해쉬태그를 입력하세요 ")
+    url = "https://instagram.com/explore/tags/" + str(keyword)
+    crawler.set_url(url)
+
+    count = int(input("게시글 갯수를 입력하세요 "))
+
+    crawler.collect_url(count)
     crawler.make_data()
     crawler.save_data("insta_test.csv")
